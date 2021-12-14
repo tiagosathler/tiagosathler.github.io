77d3a71e621093663e801fbbaee6545555509396
tiagosathler <sathler@gmail.com>
2021-12-14 13:25:07 -0300

    Adiciona o build do projeto 'Recipes-app'

--- projects/recipes-app/service-worker.js
+++ projects/recipes-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.533ebb29266891190472a0174949b35a.js"
+  "/projects/recipes-app/precache-manifest.e8b116e43d59a5bc5029d0f0be91e4db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/recipes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
