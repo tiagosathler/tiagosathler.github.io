2db36dc7d9411b342891d64ceafb3ad2f03d95b8
tiagosathler <sathler@gmail.com>
2021-12-14 13:11:20 -0300

    Adiciona o build do projeto 'Recipes-app'

---
+++ projects/recipes-app/precache-manifest.533ebb29266891190472a0174949b35a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0c0f67722519e761f049ae2e470913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53838ef29c64c1f1b11a",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "d9c44c1399d3b0735348",
+    "url": "/static/css/main.08e94d84.chunk.css"
+  },
+  {
+    "revision": "53838ef29c64c1f1b11a",
+    "url": "/static/js/2.6b352fef.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6b352fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c44c1399d3b0735348",
+    "url": "/static/js/main.023bb6a3.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  }
+]);
